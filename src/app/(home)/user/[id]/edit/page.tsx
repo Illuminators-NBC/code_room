a16a968d31042e49b5c7b9b5fbde264012fa3a13
teoyo <bigbang723@naver.com>
2024-07-09 22:40:38 +0900

    (update) layout header 생성

--- src/app/(home)/user/[id]/edit/page.tsx
+++ src/app/(home)/user/[id]/edit/page.tsx
@@ -1,12 +1,19 @@
+'use client';
+
+import Link from 'next/link';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import Image from 'next/image';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function AccountEditPage() {
+  const notify = () => {
+    toast.error('비밀번호를 확인해주세요.');
+  };
   return (
     <div className="bg-zinc-950 w-screen h-screen">
       <div className="text-white bg-zinc-950 w-[640px] h-screen border border-zinc-800 m-auto p-[30px] text-center ">
-        <p>마이페이지 프로필 수정</p>
         <Image
           src={'/logo_icon.png'}
           alt="code_room logo icon"
@@ -14,18 +21,19 @@ function AccountEditPage() {
           height="50"
           className="m-auto mt-[83px] mb-[28px]"
         />
-        <p className="text-xl font-bold	">
+        <p className="text-xl font-bold">
           <span>🌊 </span> 헤엄치는 피즈{' '}
-          <button className="ml-2 ">
+          <button className="ml-2">
             <Image src={'/refresh_icon.png'} alt="refresh icon" width="16" height="16" />
           </button>
         </p>
         <div>
-          <form className="mt-8">
+          <form className="mt-[60px]">
             <Input
-              className="w-96 h-10 bg-[#27272A] border-zinc-600 p-4 m-auto mb-7 text-white"
+              className="w-96 h-10 bg-[#71717A] border-zinc-600 p-4 m-auto mb-7 text-black placeholder:text-white placeholder:font-nomal"
               type="email"
               placeholder="Email@email.com"
+              disabled
             />
             <Input
               className="w-96 h-10 bg-[#27272A] border-zinc-600 p-4 m-auto mb-7 text-white"
@@ -40,16 +48,35 @@ function AccountEditPage() {
             <Button
               className="w-96 h-10 mt-8 bg-[#DD268E] border-0 font-bold hover:bg-[#FB2EA2] hover:text-white"
               variant="outline"
+              type="submit"
+              onClick={notify}
             >
               수 정
             </Button>
-            <Button
-              className="w-96 h-10 mt-5 bg-[#27272A] border-0 font-bold hover:bg-[#2d2d30] hover:text-white"
-              variant="outline"
-            >
-              취 소
-            </Button>
+            <Link href={'/user/1'}>
+              <Button
+                className="w-96 h-10 mt-5 bg-[#27272A] border-0 font-bold hover:bg-[#2d2d30] hover:text-white"
+                variant="outline"
+              >
+                취 소
+              </Button>
+            </Link>
           </form>
+        </div>
+        <div>
+          <ToastContainer
+            position="top-center"
+            autoClose={3000}
+            limit={2}
+            hideProgressBar={false}
+            newestOnTop={false}
+            closeOnClick
+            rtl={false}
+            pauseOnFocusLoss
+            draggable
+            pauseOnHover
+            theme="dark"
+          />
         </div>
       </div>
     </div>
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import Providers from '@/components/Providers';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
 import './globals.css';
+import Image from 'next/image';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -17,7 +19,29 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>
+      <body className="w-screen p-0 bg-zinc-950">
+        <header>
+          <div className="w-[640px] h-[53px] m-auto bg-zinc-950 items-center justify-between px-[30px] flex border border-zinc-800">
+            <Image src={'/logo.png'} alt="logo" width="93" height="14" />
+            <div className="로그인O flex">
+              <Link href={'/user'}>
+                <Image src={'/mypage_icon.png'} alt="mypage" width="16" height="20" />
+              </Link>
+              <Link href={'/'} className="ml-5">
+                <Image src={'/logout_icon.png'} alt="mypage" width="20" height="20" />
+              </Link>
+            </div>
+
+            {/* <div className="로그인X">
+              <Link href={'/login'}>
+                <Image src={'/login_icon.png'} alt="mypage" width="16" height="20" />
+              </Link>
+              <Link href={'/sign-up'}>
+                <Image src={'/signup_icon.png'} alt="mypage" width="20" height="20" />
+              </Link>
+            </div> */}
+          </div>
+        </header>
         <Providers>{children}</Providers>
       </body>
     </html>
