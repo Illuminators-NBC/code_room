a9fbf95a96060a88d09dc042fe612bcac87c517c
kybaq <flfsk12@gmail.com>
2024-07-09 21:22:56 +0900

    feat: 메인페이지 레이아웃 설정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,14 @@
+import FeedList from '@/components/HomePage/FeedList';
+
 function HomePage() {
   return (
-    <main className="mx-auto max-w-92 sm:max-w-screen-sm h-screen bg-black">
+    <main className="text-white bg-zinc-950 mx-auto max-w-92 sm:max-w-screen-sm h-screen">
       <section>
         <input type="text" placeholder="아이디어를 공유해보세요!" disabled />
       </section>
-      <section></section>
+      <section>
+        <FeedList />
+      </section>
     </main>
   );
 }
