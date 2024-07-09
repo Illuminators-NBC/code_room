809105ab893404ba44c5cf73657b93542ac8ab46
박원빈 <harry21@kakao.com>
2024-07-09 12:42:35 +0900

    (add) getPostById

--- src/app/(home)/post/[id]/page.tsx
+++ src/app/(home)/post/[id]/page.tsx
@@ -1,4 +1,14 @@
-function PostPage() {
+import { createClient } from '@/supabase/server';
+
+const getPostById = async (id: string) => {
+  const supabaseClient = createClient();
+  const { data } = await supabaseClient.from('post').select('*').eq('user_id', id);
+  return data;
+};
+
+async function PostPage({ params }: { params: { id: string } }) {
+  const { id } = params;
+  const initialData = await getPostById(id);
   return <div>PostPage</div>;
 }
 
