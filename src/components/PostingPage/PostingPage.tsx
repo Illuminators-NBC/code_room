e72eb15de19fb862b8ce649d3d33046d1917f91e
Joohwan Seo <songqkaoswe@naver.com>
2024-07-09 21:04:13 +0900

    update: dropdown 부분완료

--- src/components/PostingPage/PostingPage.tsx
+++ src/components/PostingPage/PostingPage.tsx
@@ -1,38 +1,8 @@
 'use client';
 import Image from 'next/image';
 import React, { useState } from 'react';
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuSeparator,
-  DropdownMenuTrigger
-} from '../ui/dropdown-menu';
-
+import DropdouwnMenu from './Dropdown';
 const PostingPage = () => {
-  // 예시 카테고리 목록
-  const categories = [
-    'JavaScript',
-    'React.js',
-    'Next.js',
-    'Node.js',
-    'TypeScript',
-    'Tailwind',
-    'Zustand',
-    'CSS',
-    'Recoil',
-    'Zotai',
-    'TanStack Query'
-  ];
-
-  // 선택된 카테고리 상태 관리
-  const [selectedCategory, setSelectedCategory] = useState<string>('');
-
-  // 카테고리 선택 핸들러
-  const handleCategorySelect = (category: string) => {
-    setSelectedCategory(category);
-  };
-
   return (
     <div className="bg-black min-h-screen">
       <div className="bg-black">
@@ -44,27 +14,9 @@ const PostingPage = () => {
       <main className="container mx-auto px-4">
         <div className="flex-col items-center  mb-2">
           <Image src="/terminal.png" alt="terminal" width={40} height={40} className="mb-4" />
-          <DropdownMenu>
-            <DropdownMenuTrigger className="text-pink-600 hover:text-pink-300 transition-colors duration-200">
-              Categories
-            </DropdownMenuTrigger>
-            <DropdownMenuContent>
-              <DropdownMenuSeparator />
-              <DropdownMenuItem>JavaScript</DropdownMenuItem>
-              <DropdownMenuItem>React.js</DropdownMenuItem>
-              <DropdownMenuItem>Next.js</DropdownMenuItem>
-              <DropdownMenuItem>Node.js</DropdownMenuItem>
-              <DropdownMenuItem>Node.js</DropdownMenuItem>
-              <DropdownMenuItem>Node.js</DropdownMenuItem>
-              <DropdownMenuItem>Node.js</DropdownMenuItem>
-              <DropdownMenuItem>Node.js</DropdownMenuItem>
-              <DropdownMenuItem>Node.js</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          <DropdouwnMenu />
         </div>
-        <div className="text-white mb-4">{selectedCategory || ''}</div>
-        <div className="text-white text-2xl font-bold mb-6">게시글 작성 페이지</div>
-        {/* 여기에 게시글 작성 폼을 추가할 수 있습니다 */}
+        <p className="text-white">게시글</p>
       </main>
     </div>
   );
