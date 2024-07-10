707a29dceab7a0eda393677d8fd8cb7a195cffd7
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-07-10 12:02:03 +0900

    feature: login, signup feature

--- src/supabase/middleware.ts
+++ src/supabase/middleware.ts
@@ -29,17 +29,23 @@ export async function updateSession(request: NextRequest) {
     data: { user }
   } = await supabase.auth.getUser();
 
-  // if (
-  //   !user &&
-  //   request.nextUrl.pathname !== '/' &&
-  //   !request.nextUrl.pathname.startsWith('/login') &&
-  //   !request.nextUrl.pathname.startsWith('/sign-up') &&
-  //   !request.nextUrl.pathname.startsWith('/api')
-  // ) {
-  //   const url = request.nextUrl.clone();
-  //   url.pathname = '/sign-up';
-  //   return NextResponse.redirect(url);
-  // }
+  if (
+    !user &&
+    request.nextUrl.pathname !== '/' &&
+    !request.nextUrl.pathname.startsWith('/login') &&
+    !request.nextUrl.pathname.startsWith('/sign-up') &&
+    !request.nextUrl.pathname.startsWith('/api')
+  ) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/sign-up';
+    return NextResponse.redirect(url);
+  }
+
+  if (user && (request.nextUrl.pathname.startsWith('/login') || request.nextUrl.pathname.startsWith('/sign-up'))) {
+    const url = request.nextUrl.clone();
+    url.pathname = '/';
+    return NextResponse.redirect(url);
+  }
 
   return supabaseResponse;
 }
