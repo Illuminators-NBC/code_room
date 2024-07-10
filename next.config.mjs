5dace61c79b0c072388070085f363b159952b85b
kybaq <flfsk12@gmail.com>
2024-07-10 14:35:13 +0900

    fix: Image 컴포넌트 사용 위해 이미지 서버 주소 추가

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.pixabay.com',
+        port: '',
+        pathname: '/photo/**'
+      }
+    ]
+  }
+};
 
 export default nextConfig;
