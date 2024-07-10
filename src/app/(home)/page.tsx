c2b8057cf00fd42ad20dd54be7661f88cb43c140
kybaq <flfsk12@gmail.com>
2024-07-10 18:56:42 +0900

    fix: <li> 태그 위치 수정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -2,11 +2,12 @@ import FeedList from '@/components/HomePage/FeedList';
 
 function HomePage() {
   return (
-    <main className="text-white bg-zinc-950 mx-auto max-w-92 sm:max-w-screen-sm h-screen">
+    <main className="text-white bg-zinc-950 mx-auto max-w-92 p-3.5 sm:max-w-screen-sm sm:p-7 h-screen">
       <section>
         <input type="text" placeholder="아이디어를 공유해보세요!" disabled />
       </section>
-      <section>
+      <section className="w-full">
+        {/* tanstack query 의 prefetchQuery 를 사용하면 SEO 최적화에 도움이 됨*/}
         <FeedList />
       </section>
     </main>
