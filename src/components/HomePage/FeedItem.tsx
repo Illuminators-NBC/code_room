a1c4aa613bc53ad336d37ff3e15b8d55db133090
kybaq <flfsk12@gmail.com>
2024-07-10 18:58:14 +0900

    fix: <li> 태그 추가

--- src/components/HomePage/FeedItem.tsx
+++ src/components/HomePage/FeedItem.tsx
@@ -1,6 +1,17 @@
 import { PostProps } from '@/types/posts';
+import Image from 'next/image';
 import React from 'react';
 
 export default function FeedItem({ post }: PostProps) {
-  return <div>{post.content}</div>;
+  return (
+    <li className="max-w-92 sm:max-w-120">
+      <h6>닉: {post.nickname}</h6>
+      {post.image ? (
+        <figure className="relative max-w-92 sm:max-w-120 h-32 sm:h-64">
+          사진: <Image priority={true} src={post.image} alt="유저가 업로드한 사진" fill={true} sizes="100vw" />
+        </figure>
+      ) : null}
+      <p>내용: {post.content}</p>
+    </li>
+  );
 }
