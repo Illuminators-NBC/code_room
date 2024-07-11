784edbcca534625e8c0c40423dbd4e05e6bdc1c1
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-07-11 16:47:39 +0900

    feature: zustand nickname & user ID

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,10 +1,16 @@
+'use client';
+import AuthObserver from '@/components/common/AuthObserver';
 import LogoutButton from '@/components/LoginPage/LogoutButton';
+import useUserInfo from '@/hooks/useUserInfo';
 
 function HomePage() {
+  const { userInfo } = useUserInfo();
   return (
     <>
       <div>HomePage</div>
       <LogoutButton />
+      <p>Nickname: {userInfo.nickname}</p>
+      <p>Id: {userInfo.id}</p>
     </>
   );
 }
