3257a051d9385a1c63068d07ccccf299c9f96748
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-07-11 19:09:07 +0900

    fix: (home)page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,14 +3,22 @@ import AuthObserver from '@/components/common/AuthObserver';
 import LogoutButton from '@/components/LoginPage/LogoutButton';
 import useUserInfo from '@/hooks/useUserInfo';
 
+import FeedList from '@/components/HomePage/FeedList';
+import PostBox from '@/components/HomePage/PostBox';
+
 function HomePage() {
   const { userInfo } = useUserInfo();
   return (
     <>
-      <div>HomePage</div>
       <LogoutButton />
-      <p>Nickname: {userInfo.nickname}</p>
-      <p>Id: {userInfo.id}</p>
+      <header></header>
+      <main className="text-white mx-auto max-w-92 p-3.5 sm:max-w-screen-sm sm:p-7">
+        <PostBox />
+        <section className="w-full">
+          {/* tanstack query 의 prefetchQuery 를 사용하면 SEO 최적화에 도움이 됨*/}
+          <FeedList />
+        </section>
+      </main>
     </>
   );
 }
