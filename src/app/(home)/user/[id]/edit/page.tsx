a355773a1c8c1e7611061b4ff904713c768dcd99
teoyo <bigbang723@naver.com>
2024-07-11 17:26:28 +0900

    (fix) 닉네임 랜덤 변경 후 저장

--- src/app/(home)/user/[id]/edit/page.tsx
+++ src/app/(home)/user/[id]/edit/page.tsx
@@ -20,7 +20,6 @@ type ChangePasswordFormProps = {
 const AccountEditPage: React.FC<ChangePasswordFormProps> = ({ onSubmit }) => {
   const supabase = createClient();
   const [userEmail, setUserEmail] = useState('');
-  const [userNickname, setUserNickName] = useState('');
   const [newPassword, setNewPassword] = useState('');
   const [confirmPassword, setConfirmPassword] = useState('');
   const initialState: FormState = {
@@ -29,6 +28,48 @@ const AccountEditPage: React.FC<ChangePasswordFormProps> = ({ onSubmit }) => {
     confirmPw: '',
     nickname: '',
     recaptchaToken: ''
+  };
+
+  // 데이타 불러오기
+  useEffect(() => {
+    const fetchUserData = async () => {
+      try {
+        const {
+          data: { user },
+          error: userError
+        } = await supabase.auth.getUser();
+        if (userError) throw userError;
+        if (user) {
+          const { data, error } = await supabase.from('user').select('email, nickname').eq('id', user.id).single();
+          if (error) throw error;
+          if (data) {
+            console.log(data);
+            setUserEmail(data.email ?? '');
+            handleNicknameChange(data.nickname ?? '');
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchUserData();
+  }, []);
+
+  // 닉네임 업데이트
+  const updateNickname = async (e) => {
+    e.preventDefault();
+    try {
+      const { data, error } = await supabase
+        .from('user')
+        .update({ nickname: formState.nickname })
+        .eq('email', userEmail);
+      console.log(data);
+      if (error) {
+        throw error;
+      }
+    } catch (error) {
+      toast.error('닉네임을 수정하는 도중 오류가 발생했습니다.');
+    }
   };
   const handleNicknameGenerated = (nickname: string) => {
     setFormState((prev) => ({ ...prev, nickname }));
@@ -42,46 +83,16 @@ const AccountEditPage: React.FC<ChangePasswordFormProps> = ({ onSubmit }) => {
     setFormState((prev) => ({ ...prev, nickname }));
   };
 
+  // 업데이트
   const handleSubmit = async (e) => {
     e.preventDefault();
     setNewPassword(e.target.value);
 
     const { data: user, error } = await supabase.auth.updateUser({ password: newPassword });
+
+    updateNickname(e);
     toast.success('수정되었습니다.');
     console.log(error);
-  };
-
-  useEffect(() => {
-    const fetchUserData = async () => {
-      try {
-        const {
-          data: { user },
-          error: userError
-        } = await supabase.auth.getUser();
-        if (userError) throw userError;
-        if (user) {
-          const { data, error } = await supabase.from('user').select('email, nickname').eq('id', user.id).single();
-          if (error) throw error;
-          if (data) {
-            setUserEmail(data.email ?? '');
-            setUserNickName(data.nickname ?? '');
-          }
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    fetchUserData();
-  }, []);
-
-  // 닉네임 바꾸기
-  const updateNickname: React.MouseEventHandler<HTMLElement> = (e) => {
-    e.preventDefault();
-    // 새로고침 버튼을 누르면 닉네임이 랜덤 변경되는 함수 실행
-
-    return toast.success('수정되었습니다.');
-    // 변경된 닉네임은 setnickname -> nickname으로 저장
-    // nickname을 supabase에 업데이트
   };
 
   return (
@@ -113,7 +124,6 @@ const AccountEditPage: React.FC<ChangePasswordFormProps> = ({ onSubmit }) => {
               placeholder="password"
               value={newPassword}
               onChange={(e) => setNewPassword(e.target.value)}
-              required
             />
             <Input
               className="w-96 h-10 bg-[#27272A] border-zinc-600 p-4 m-auto mb-7 text-white"
@@ -123,7 +133,6 @@ const AccountEditPage: React.FC<ChangePasswordFormProps> = ({ onSubmit }) => {
               onChange={(e) => {
                 setConfirmPassword(e.target.value);
               }}
-              required
             />
             <Button
               className="w-96 h-10 mt-8 bg-[#DD268E] border-0 font-bold hover:bg-[#FB2EA2] hover:text-white"
