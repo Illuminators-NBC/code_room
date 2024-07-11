cf88c68d7afe5d87537d9c2d491be7aecfa08202
kybaq <flfsk12@gmail.com>
2024-07-11 14:30:21 +0900

    style: 기본 레이아웃 설정

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,16 +1,18 @@
 import FeedList from '@/components/HomePage/FeedList';
+import PostBox from '@/components/HomePage/PostBox';
 
 function HomePage() {
   return (
-    <main className="text-white bg-zinc-950 mx-auto max-w-92 p-3.5 sm:max-w-screen-sm sm:p-7 h-screen">
-      <section>
-        <input type="text" placeholder="아이디어를 공유해보세요!" disabled />
-      </section>
-      <section className="w-full">
-        {/* tanstack query 의 prefetchQuery 를 사용하면 SEO 최적화에 도움이 됨*/}
-        <FeedList />
-      </section>
-    </main>
+    <>
+      <header></header>
+      <main className="text-white mx-auto max-w-92 p-3.5 sm:max-w-screen-sm sm:p-7">
+        <PostBox />
+        <section className="w-full">
+          {/* tanstack query 의 prefetchQuery 를 사용하면 SEO 최적화에 도움이 됨*/}
+          <FeedList />
+        </section>
+      </main>
+    </>
   );
 }
 
