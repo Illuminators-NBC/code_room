91055b01b617878821330d653611b2c6f71ceea4
JvnKim <166312623+JvnKim@users.noreply.github.com>
2024-07-13 18:07:00 +0900

    fix: nickname changing

--- src/components/common/AuthObserver.ts
+++ src/components/common/AuthObserver.ts
@@ -8,22 +8,45 @@ function AuthObserver() {
   const supabase = createClient();
   const { setUserInfo, deleteUserInfo } = useUserInfo();
 
-  useEffect(() => {
-    const session = supabase.auth.getSession();
+  const getUserNickname = async (userId: string) => {
+    const { data, error } = await supabase.from('user').select('nickname').eq('id', userId).single();
 
-    const handleAuthChange = (event: any, session: any) => {
+    if (error) {
+      return null;
+    }
+
+    return data?.nickname;
+  };
+
+  useEffect(() => {
+    // const session = supabase.auth.getSession();
+
+    // const handleAuthChange = (event: any, session: any) => {
+    //   if (session?.user) {
+    //     setUserInfo({
+    //       id: session.user.id,
+    //       nickname: session.user.user_metadata.nickname,
+    //       email: session.user.user_metadata.email
+    //     });
+    //   } else {
+    //     deleteUserInfo();
+    //   }
+    // };
+
+    // handleAuthChange(null, session);
+
+    const handleAuthChange = async (event: any, session: any) => {
       if (session?.user) {
+        const nickname = await getUserNickname(session.user.id);
         setUserInfo({
           id: session.user.id,
-          nickname: session.user.user_metadata.nickname,
+          nickname: nickname,
           email: session.user.user_metadata.email
         });
       } else {
         deleteUserInfo();
       }
     };
-
-    handleAuthChange(null, session);
 
     const {
       data: { subscription }
