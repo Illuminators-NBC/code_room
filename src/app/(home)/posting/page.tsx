4ee4d2b1d56bd970531c93937a5cf55430f4c1e7
박원빈 <harry21@kakao.com>
2024-07-15 10:07:55 +0900

    (design) PostingPage: change Header layout

--- src/app/(home)/posting/page.tsx
+++ src/app/(home)/posting/page.tsx
@@ -1,12 +1,9 @@
 'use client';
-import Header from '@/components/common/Header';
 import PostingPage from '@/components/PostingPage';
-import useUserInfo from '@/hooks/useUserInfo';
 
 function PostPage() {
   return (
     <div>
-      <Header />
       <PostingPage />
     </div>
   );
--- src/components/PostingPage/PostingPage.tsx
+++ src/components/PostingPage/PostingPage.tsx
@@ -1,9 +1,10 @@
-import Image from 'next/image';
+import Header from '../common/Header';
 import { Posting } from './Posting/Posting';
 
 const PostingPage = () => {
   return (
-    <div className="bg-zinc-950 w-[640px] h-screen border-2 border-zinc-800 h-auto m-auto">
+    <div className="bg-zinc-950 w-[640px] h-screen border-2 border-zinc-800 m-auto">
+      <Header />
       <main className="container mx-1 px-4 mt-[30px]">
         <Posting />
       </main>
